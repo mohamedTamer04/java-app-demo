36440837ece8d550c16046289167e0196e0c7a89
mohamedTamer04 <mt0991787@gmail.com>
2024-08-29 12:21:55 +0300

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        for(int 1 =0 ; i < 20 ; i++){
+        for(int i =0 ; i < 20 ; i++){
             System.out.println("Hello world!");
     }
 }
